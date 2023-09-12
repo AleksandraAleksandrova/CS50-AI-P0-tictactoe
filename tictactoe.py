1b8ec6ad9bd88909d65290e3422a5a7d48f804b9
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-09-12 18:39:20 +0300

    add winner and utility

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -52,14 +53,34 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    resultBoard = copy.deepcopy(board)
+    if action not in actions(board):
+        raise Exception("Invalid action")
+    else:
+        resultBoard[action[0]][action[1]] = player(board)
+    return resultBoard
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check rows
+    for row in board:
+        if row[0] == row[1] == row[2] != EMPTY:
+            return row[0]
+
+    # Check columns
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    elif board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+
+    return None      
 
 
 def terminal(board):
@@ -73,7 +94,12 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
